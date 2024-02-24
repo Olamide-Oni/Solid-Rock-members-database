11eee2b2b1773f1509a0cffd156ae572b8701500
Olamide-Oni <122795157+Olamide-Oni@users.noreply.github.com>
2024-02-24 16:51:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function submitForm() {
  
   function displayCongratulationsMessage() {
    
-    document.body.classList.add('blurred');
+    document.getElementById("container").classList.add('blurred');
+    /*document.body.classList.add('blurred');*/ 
 
     const congratulationsDiv = document.createElement('div');
     congratulationsDiv.textContent = 'Congratulations! Your form has been successfully submitted.';
@@ -44,12 +45,28 @@ function submitForm() {
     
     document.body.appendChild(errorDiv);
   }
-
- const form = document.getElementById('form-container');
-
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // 
   
-  submitForm();
-})
+  const form = document.getElementById("form-container");
+
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  fetch(this.action, {
+    method: 'POST',
+    body: new FormData(this)
+  })
+  .then(response => {
+    if (response.ok) {
+      displayCongratulationsMessage();
+    } else {
+      displayErrorMessage('Form submission failed. Please try again later.');
+    }
+  })
+  .catch(error => {
+    
+    console.error('Error:', error);
+    // Handle error if needed
+    displayErrorMessage('An unexpected error occurred. Please try again later.');
+  });
+}) 
